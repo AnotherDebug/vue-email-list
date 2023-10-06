b19f2e57c031978a10d682c39663d610623f3a27
AnotherDebug <antonio.faddanno@gmail.com>
2023-10-06 17:13:58 +0200

    [ADD]: loop for e push data response

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,16 +21,8 @@ createApp({
     return {
       url: "https://flynn.boolean.careers/exercises/api/random/mail",
 
-      firstEmail: "",
-      secondEmail: "",
-      thirdEmail: "",
-      fourthEmail: "",
-      fifthEmail: "",
-      sixthEmail: "",
-      seventhEmail: "",
-      eighthEmail: "",
-      ninthEmail: "",
-      titheEmail: "",
+      emails: [],
+      emailNums: 10,
     };
   },
 
@@ -38,115 +30,18 @@ createApp({
 
     getApi() {
 
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.firstEmail = response.data.response;
-          console.log(this.firstEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.secondEmail = response.data.response;
-          console.log(this.secondEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.thirdEmail = response.data.response;
-          console.log(this.thirdEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.fourthEmail = response.data.response;
-          console.log(this.fourthEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.fifthEmail = response.data.response;
-          console.log(this.fifthEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.sixthEmail = response.data.response;
-          console.log(this.sixthEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.seventhEmail = response.data.response;
-          console.log(this.seventhEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.eighthEmail = response.data.response;
-          console.log(this.eighthEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.ninthEmail = response.data.response;
-          console.log(this.ninthEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-
-      //Richiesta email da API
-      axios
-        .get(this.url)
-        .then((response) => {
-          this.titheEmail = response.data.response;
-          console.log(this.titheEmail);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
+     for(let i = 0; i < this.emailNums; i++) {
+       //Richiesta email da API
+       axios
+       .get(this.url)
+       .then((response) => {
+         this.emails.push(response.data.response);
+         console.log(this.emails);
+       })
+       .catch((error) => {
+         console.log(error);
+       });
+     };
 
 
     },
@@ -156,5 +51,6 @@ createApp({
   mounted() {
     console.log("Prova");
     this.getApi();
+    console.log(this.emails);
   },
 }).mount("#app");
